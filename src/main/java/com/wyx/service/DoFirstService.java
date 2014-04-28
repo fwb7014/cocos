267cfda641d8977f0bf458fc9dd2ecbb5e25e8e5
fwb7014 <258982902@qq.com>
2014-04-28 22:05:00 +0800

    提交代码

---
+++ src/main/java/com/wyx/service/DoFirstService.java
@@ -0,0 +1,49 @@
+package com.wyx.service;
+
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.wyx.web.ReceiveController;
+import com.wyx.web.util.SecurityUtil;
+import com.wyx.web.util.WebProperties;
+
+@Service
+public class DoFirstService {
+
+	private static final Logger logger = Logger.getLogger(ReceiveController.class);
+	
+	public String doFirstService(String signature, String timestamp, String nonce,
+			String echostr) {
+		String[] str = {WebProperties.token,timestamp, nonce};
+		Arrays.sort(str);
+		StringBuffer bf = new StringBuffer();
+		for(String temp:str){
+			bf.append(temp);
+		}
+		if (checkSignature(bf.toString(),signature)) {
+			System.out.println("通过返回"+echostr);
+			return echostr;
+		}
+		return "";
+
+	}
+
+	/**
+	 * 检验加密是否正确
+	 * 
+	 * @param sourceParam
+	 * @param signature
+	 * @return
+	 */
+	private boolean checkSignature(String sourceParam, String signature) {
+		logger.info("开始进行加密检验");
+		logger.info("原文" + sourceParam);
+		String securityStr = SecurityUtil.SHA1(sourceParam);
+		logger.info("自己计算的密文" + securityStr);
+		logger.info("接收的密文" + signature);
+		return securityStr.equals(signature);
+	}
+
+}
---
+++ src/main/java/com/wyx/web/ReceiveController.java
@@ -0,0 +1,53 @@
+package com.wyx.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wyx.service.DoFirstService;
+
+@Controller
+public class ReceiveController {
+	
+	private static final Logger logger = Logger.getLogger(ReceiveController.class);
+	
+	@Autowired
+	private DoFirstService doFirst ;
+
+	/**
+	 * 开通的第一步 激活开发者模式
+	 * 
+	 * @param signature
+	 * @param timestampba 
+	 * 
+	 * @param nonce
+	 * @param echostr
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("receive.do")
+	public String ktWinXin(HttpServletRequest request) {
+		return doFirstService(request);
+	}
+	
+	/**
+	 * 验证
+	 * @param request
+	 * @return
+	 */
+	public String doFirstService(HttpServletRequest request){
+		String signature =request.getParameter("signature");
+		String timestamp =request.getParameter("timestamp");
+		String nonce =request.getParameter("nonce");
+		String echostr =request.getParameter("echostr");
+		logger.info("timestamp="+timestamp+",nonce="+nonce+",echostr="+echostr+",signature="+signature);
+		return doFirst.doFirstService(signature, timestamp, nonce, echostr);
+		
+		
+	}
+	
+}
