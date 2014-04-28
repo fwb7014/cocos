943af03f949b74ea471954380d4ae857589c7889
fwb7014 <258982902@qq.com>
2014-04-28 18:40:52 +0800

    修改了加密算法

--- src/main/java/com/wyx/web/util/SecurityUtil.java
+++ src/main/java/com/wyx/web/util/SecurityUtil.java
@@ -5,38 +5,37 @@ import java.security.NoSuchAlgorithmException;
 
 /**
  * 加密的处理方法
+ * 
  * @author Administrator
- *
+ * 
  */
 public class SecurityUtil {
-	
+
 	public static String SHA1(String inStr) {
-        MessageDigest md = null;
-        String outStr = null;
-        try {
-            md = MessageDigest.getInstance("SHA-1");     //选择SHA-1，也可以选择MD5
-            byte[] digest = md.digest(inStr.getBytes());       //返回的是byet[]，要转化为String存储比较方便
-            outStr = bytetoString(digest);
-        }
-        catch (NoSuchAlgorithmException nsae) {
-            nsae.printStackTrace();
-        }
-        return outStr;
-    }
-	
-	public static String bytetoString(byte[] digest) {
-        String str = "";
-        String tempStr = "";
-        
-        for (int i = 1; i < digest.length; i++) {
-            tempStr = (Integer.toHexString(digest[i] & 0xff));
-            if (tempStr.length() == 1) {
-                str = str + "0" + tempStr;
-            }
-            else {
-                str = str + tempStr;
-            }
-        }
-        return str.toLowerCase();
-    }
+		MessageDigest md = null;
+		String outStr = null;
+		try {
+			md = MessageDigest.getInstance("SHA-1"); // 选择SHA-1，也可以选择MD5
+			byte[] digest = md.digest(inStr.getBytes()); // 返回的是byet[]，要转化为String存储比较方便
+			outStr = byte2Hex(digest);
+		} catch (NoSuchAlgorithmException nsae) {
+			nsae.printStackTrace();
+		}
+		return outStr;
+	}
+
+
+	/**
+	 * 将字节数组转化为16进制字符串
+	 * @param srcBytes
+	 * @return
+	 */
+	private static String byte2Hex(byte[] srcBytes) {
+		StringBuilder hexRetSB = new StringBuilder();
+		for (byte b : srcBytes) {
+			String hexString = Integer.toHexString(0x00ff & b);
+			hexRetSB.append(hexString.length() == 1 ? 0 : "").append(hexString);
+		}
+		return hexRetSB.toString();
+	}
 }
